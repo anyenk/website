a4eeca84f07df599469c7076430166909da61def
Anibal Catalan <n1mr0d@tuta.io>
2026-01-12 15:41:29 -0300

    add sg to wordpress ec

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "wordpress" {
   ami           = "ami-02e6c834d44f57b3e" # Amazon Linux 2023 - sa-east-1
   instance_type = "t3.micro"
   subnet_id     = data.aws_subnet.public_a.id
+  vpc_security_group_ids = [aws_security_group.wordpress.id]
 
   tags = {
     Name = "wordpress-prod"
