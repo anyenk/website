38096bc1ee5be1f1188b82bbff7993281a146450
Anibal Catalan <n1mr0d@tuta.io>
2026-01-12 15:24:05 -0300

    add sg

---
+++ terraform/sg.tf
@@ -0,0 +1,69 @@
+resource "aws_security_group" "wordpress" {
+  name        = "wordpress-sg"
+  description = "Security group for WordPress"
+  vpc_id      = data.aws_vpc.main.id
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["TU_IP/32"] # Tu IP para administración
+  }
+
+  # HTTP desde Cloudflare
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = [
+      "173.245.48.0/20",
+      "103.21.244.0/22",
+      "103.22.200.0/22",
+      "103.31.4.0/22",
+      "141.101.64.0/18",
+      "108.162.192.0/18",
+      "190.93.240.0/20",
+      "188.114.96.0/20",
+      "197.234.240.0/22",
+      "198.41.128.0/17",
+      "162.158.0.0/15",
+      "104.16.0.0/13",
+      "104.24.0.0/14",
+      "172.64.0.0/13",
+      "131.0.72.0/22"
+    ]
+  }
+
+  # HTTPS desde Cloudflare
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = [
+      "173.245.48.0/20",
+      "103.21.244.0/22",
+      "103.22.200.0/22",
+      "103.31.4.0/22",
+      "141.101.64.0/18",
+      "108.162.192.0/18",
+      "190.93.240.0/20",
+      "188.114.96.0/20",
+      "197.234.240.0/22",
+      "198.41.128.0/17",
+      "162.158.0.0/15",
+      "104.16.0.0/13",
+      "104.24.0.0/14",
+      "172.64.0.0/13",
+      "131.0.72.0/22"
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wordpress-sg"
+  }
+}
